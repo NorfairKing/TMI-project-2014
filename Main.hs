bbb9314efe90d85f27e74b77f965438d1a6c4d71
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2014-03-29 15:19:52 +0100

    tried to fix an error, failed...

--- Main.hs
+++ Main.hs
@@ -9,21 +9,21 @@ import Text.Printf
 
 naive :: [Circle] -> [Position]
 naive circles =
-  return "Dit algoritme is niet geïmplementeerd."
+  error "Dit algoritme is niet geïmplementeerd."
 
 scanline_quadratic :: [Circle] -> [Position]
 scanline_quadratic circles =
-  return "Dit algoritme is niet geïmplementeerd."
+  error "Dit algoritme is niet geïmplementeerd."
 
 scanline_linearithmic :: [Circle] -> [Position]
 scanline_linearithmic circles =
-  return "Dit algoritme is niet geïmplementeerd."
+  error "Dit algoritme is niet geïmplementeerd."
 
 
-mkCircle :: Scalar -> Scalar -> Scalar -> IO Circle
-mkCircle x y r = return ((x,y),r)
+mkCircle :: Scalar -> Scalar -> Scalar -> Circle
+mkCircle x y r = ((Pos x y),r)
 
-solve :: Int -> [Circle] -> [Point]
+solve :: Int -> [Circle] -> [Position]
 solve a c | a == 1 = naive c
           | a == 2 = scanline_quadratic c
           | a == 3 = scanline_linearithmic c
@@ -32,11 +32,11 @@ main = do
   algorithm <- parseLine
   nCircles <- parseLine
   circles <- replicateM nCircles parseLine
-  map mkCircle circles
+  let cs = map mkCircle circles
   start <- getCPUTime
-  solution <- solve algorithm circles
+  solution <- solve algorithm cs
   end   <- getCPUTime
   let diff = (fromIntegral (end - start)) / (10^9)
-  putStrLn solution
+  putStrLn $ show solution
   printf "Computation time: %0.3f ms\n" (diff :: Double)
   
