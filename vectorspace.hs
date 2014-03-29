3b8912141291262fc3e74893e7ddf57dd7678878
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2014-03-29 11:38:43 +0100

    prettying stuff up

--- vectorspace.hs
+++ vectorspace.hs
@@ -1,19 +1,22 @@
+-- We need this for reasons.
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 -- Define a euclidean vector space with...
-class Vector a where
+class Vector a b where
     -- Vector addition
     (<+>) :: a -> a -> a
     -- Vector substraction
     (<->) :: a -> a -> a
     -- Scalar multiplication
-    (<*>) :: Double -> a -> a
+    (<*>) :: b -> a -> a
     -- Dotproduct
-    o :: a -> a -> Double
+    o :: a -> a -> b
 
--- Define a position
+-- Define a 2D position.
 data Position = Pos Double Double
 
--- Declare that positions are vectors
-instance Vector Position where
+-- Declare that positions are vectors with doubles as scalar.
+instance Vector Position Double where
     -- Addition
     (<+>) (Pos x1 y1) (Pos x2 y2) = Pos (x1+x2) (y1+y2)
     -- Substraction 
@@ -23,10 +26,16 @@ instance Vector Position where
     -- Dotproduct
     o (Pos x1 y1) (Pos x2 y2) = x1*x2+y1*y2
 
--- Define an equality test for a position
+-- Define an equality test for a position.
 instance Eq Position where
     (==) (Pos x1 y1) (Pos x2 y2) = (x1 == x2) && (y1 == y2)
 
--- Define the string representation for a position
+-- Define the string representation for a position.
 instance Show Position where
     show (Pos x y) = "(" ++ (show x) ++ "," ++ (show y) ++ ")"
+
+-- Define the magnitude of a vector product.
+-- Note: This is not an accurate definition of a vector product,
+-- but it fits our purposes.
+x :: Position -> Position -> Double
+x (Pos x1 y1) (Pos x2 y2) = x1*y2 - y1*x2
