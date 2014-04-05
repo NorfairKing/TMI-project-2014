9e67c513c655b51d28718e127b0749d61842ee66
Xavier Goás Aguililla <xavier.goas@gmail.com>
2014-04-06 01:07:04 +0200

    and some more benchmarks

--- code/Benchmark.hs
+++ code/Benchmark.hs
@@ -28,18 +28,36 @@ benchmark = do
   -- yeah my code sucks but I tried to write macros first and it sucked balls because of the type chaos so now i'm doing this shit and it's much better
   defaultMain [
     bgroup "algorithm 1" [
-       bench "algorithm 1, 10 cs" $ nf alg1 (cs !! 0)
-       , bench "algorithm 1, 20 cs" $ nf alg1 (cs !! 1)
-       , bench "algorithm 1, 30 cs" $ nf alg1 (cs !! 2)
+       bench "10 circles" $ nf alg1 (cs !! 0)
+       , bench "20 circles" $ nf alg1 (cs !! 1)
+       , bench "30 circles" $ nf alg1 (cs !! 2)
+       , bench "40 circles" $ nf alg1 (cs !! 3)
+       , bench "50 circles" $ nf alg1 (cs !! 4)
+       , bench "60 circles" $ nf alg1 (cs !! 5)
+       , bench "70 circles" $ nf alg1 (cs !! 6)
+       , bench "80 circles" $ nf alg1 (cs !! 7)
+       , bench "90 circles" $ nf alg1 (cs !! 8)
        ]
     , bgroup "algorithm 2" [
-       bench "algorithm 2, 10 cs" $ nf alg2 (cs !! 0)
-       , bench "algorithm 2, 20 cs" $ nf alg2 (cs !! 1)
-       , bench "algorithm 2, 30 cs" $ nf alg2 (cs !! 2)
+       bench "10 circles" $ nf alg2 (cs !! 0)
+       , bench "20 circles" $ nf alg2 (cs !! 1)
+       , bench "30 circles" $ nf alg2 (cs !! 2)
+       , bench "40 circles" $ nf alg2 (cs !! 3)
+       , bench "50 circles" $ nf alg2 (cs !! 4)
+       , bench "60 circles" $ nf alg2 (cs !! 5)
+       , bench "70 circles" $ nf alg2 (cs !! 6)
+       , bench "80 circles" $ nf alg2 (cs !! 7)
+       , bench "90 circles" $ nf alg2 (cs !! 8)
        ]
     -- , bgroup "algorithm 3" [
-    --    bench "algorithm 3, 10 cs" $ nf alg3 (cs !! 0)
-    --    , bench "algorithm 3, 20 cs" $ nf alg3 (cs !! 1)
-    --    , bench "algorithm 3, 30 cs" $ nf alg3 (cs !! 2)
+    --    bench "10 circles" $ nf alg3 (cs !! 0)
+    --    , bench "20 circles" $ nf alg3 (cs !! 1)
+    --    , bench "30 circles" $ nf alg3 (cs !! 2)
+    --    , bench "40 circles" $ nf alg3 (cs !! 3)
+    --    , bench "50 circles" $ nf alg3 (cs !! 4)
+    --    , bench "60 circles" $ nf alg3 (cs !! 5)
+    --    , bench "70 circles" $ nf alg3 (cs !! 6)
+    --    , bench "80 circles" $ nf alg3 (cs !! 7)
+    --    , bench "90 circles" $ nf alg3 (cs !! 8)
     --    ]
     ]
