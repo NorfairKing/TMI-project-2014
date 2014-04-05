72034895c08eebe86853825d9e6a021c635932c0
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2014-04-05 20:56:30 +0200

    generation

--- code/Quadratic.hs
+++ code/Quadratic.hs
@@ -8,7 +8,7 @@ import Position
 intersections :: [Circle] -> [Position]
 intersections cs = nub $ go (sort $ eventPointss cs) []
     where
-        go [] act = Naive.intersections act
+        go [] act = []
         go (Insert c : evl) act = concatMap (circlesIntersections c) act ++ go evl (c:act)
         go (Delete c : evl) act = go evl (delete c act)
 
--- code/Test.hs
+++ code/Test.hs
@@ -1,5 +1,45 @@
 module Test where
 
+import Control.Monad
+import System.Random
+import System.IO
+
+import Circle
+import Position
+
+
+algorithmNrs :: [Int]
+algorithmNrs = [1..3]
+
+circlesNrs :: [Int]
+circlesNrs = [ c*10^e | e <- [0..1], c<- [1..9] ]
+
+
 test :: [String] -> IO () 
 test args = do
-    putStrLn "TODO"
+    generateTests
+
+generateTests :: IO ()
+generateTests = do
+    foldr (>>) (return ()) (map generateTestCase circlesNrs)
+
+generateTestCase :: Int -> IO ()
+generateTestCase nc = do
+    circles <- replicateM nc getRandomCircle
+    mapM_ (makeFile circles) algorithmNrs
+
+makeFile cs na = do
+    outh <- openFile ("test_input/testcase_" ++ show na ++ "_" ++ show nc ++ ".txt") WriteMode
+    hPutStrLn outh $ show na
+    hPutStrLn outh $ show nc
+    mapM_ (hPutStrLn outh . show) cs
+    hClose outh
+    where nc = length cs
+
+
+getRandomCircle :: IO Circle
+getRandomCircle = do
+    x <- randomIO :: IO Double
+    y <- randomIO :: IO Double
+    r <- randomIO :: IO Double
+    return (Cir (Pos x y) r)
