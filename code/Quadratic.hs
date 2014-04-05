47cd7a3f0b996f652e1222cb49fde09ca2eef13c
Xavier Goás Aguililla <xavier.goas@gmail.com>
2014-04-05 22:14:41 +0200

    SMARTER PATTERN MATCHING

--- code/Quadratic.hs
+++ code/Quadratic.hs
@@ -8,7 +8,7 @@ import Position
 intersections :: [Circle] -> [Position]
 intersections cs = nub $ go (sort $ eventPointss cs) []
     where
-        go [] act = []
+        go [e] act = []
         go (Insert c : evl) act = concatMap (circlesIntersections c) act ++ go evl (c:act)
         go (Delete c : evl) act = go evl (delete c act)
 
