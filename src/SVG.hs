c5c1ed52bc42f6d56d63e0ebc551f600d6cdfeae
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2014-04-06 11:55:49 +0200

    it works :D

--- src/SVG.hs
+++ src/SVG.hs
@@ -3,48 +3,66 @@ module SVG where
 import Circle
 import Position
 
+-- Settings
+scale = 250.0
+offset = 1.0
+width = 1000
+height = 1000
+background = "#000000"
+circleColor = "#ffffff"
+intersectionColor = "#ff0000"
+
 class Drawable a where
     draw :: a -> String
 
-scale = 1000.0
-
-data SVGUtil =    Header 
+data SVGUtil = Header
                 | Background
                 | Footer
 
 instance Drawable SVGUtil where
     draw Header
-        = "<svg width=\"1920\" height=\"1080\">"
+        = "<svg "
+            ++ " width=\"" ++ show width ++ "\""
+            ++ " height=\"" ++ show height ++ "\""
+            ++ " >"
     
     draw Background
-        = "    <rect width=\"100%\" height=\"100%\" fill=\"black\"/>"
+        = " <rect "
+            ++ " width=\"100%\""
+            ++ " height=\"100%\""
+            ++ " fill=\"" ++ background ++ "\""
+            ++ " />"
     
     draw Footer
         = "</svg>"
 
+
+
 -- Define a circle to be drawable
 instance Drawable Circle where
     draw (Cir (Pos x y) r)
-        =   "    <circle"
-         ++ " cx=\"" ++ show x' ++ "\""
-         ++ " cy=\"" ++ show y' ++ "\""
-         ++ " r=\""  ++ show r' ++ "\""
-         ++ " stroke=\"white\""
-         ++ " stroke-width=\"1\""
-         ++ " style=\"fill-opacity:0.0;stroke-opacity:1\""
-         ++ " />"
+        = " <circle"
+            ++ " cx=\"" ++ show x' ++ "\""
+            ++ " cy=\"" ++ show y' ++ "\""
+            ++ " r=\"" ++ show r' ++ "\""
+            ++ " stroke=\"" ++ circleColor ++ "\""
+            ++ " stroke-width=\"1\""
+            ++ " style=\"fill-opacity:0.0;stroke-opacity:1\""
+            ++ " />"
         where
-            [x',y',r'] = map (floor.(*SVG.scale)) [x,y,r]
+            x' = floor $ (x+offset)*scale
+            y' = floor $ (y+offset)*scale
+            r' = floor $ r*scale
 
 instance Drawable Position where
-    draw (Pos x y) = "    <circle"
-                        ++ " cx=\"" ++ show x' ++ "\""
-                        ++ " cy=\"" ++ show y' ++ "\""
-                        ++ " r=\""  ++ show r  ++ "\""
-                        ++ " fill=\"red\""
-                        ++ " />"
+    draw (Pos x y)
+        = " <circle"
+            ++ " cx=\"" ++ show x' ++ "\""
+            ++ " cy=\"" ++ show y' ++ "\""
+            ++ " r=\"" ++ show r ++ "\""
+            ++ " fill=\"" ++ intersectionColor ++ "\""
+            ++ " />"
         where
-            [x',y'] = map (floor.(*SVG.scale)) [x,y]
-            r = 3
-
-
+            x' = floor $ (x+offset)*scale
+            y' = floor $ (y+offset)*scale
+            r = 2
