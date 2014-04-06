bfa79c6760bd9dc22ffb129311e4e258aeb8baaa
Xavier Goás Aguililla <xavier.goas@gmail.com>
2014-04-07 01:51:58 +0200

    algo 3 extra refinements

--- src/Circle.hs
+++ src/Circle.hs
@@ -4,7 +4,9 @@ import Text.Printf
 
 import Position
 
-data Circle = Cir Position Double
+data Circle = Cir { centre :: Position
+                  , radius :: Double
+                  }
 
 -- Define a string representation for circles
 instance Show Circle where
--- src/Linearithmic.hs
+++ src/Linearithmic.hs
@@ -10,30 +10,32 @@ import Position
 import Vector
 
 intersections :: [Circle] -> [Position]
-intersections cs = L.nub $ concat $ A.asListL $ go (sort $ eventPointss cs) A.empty 0
+intersections cs = L.nub $ concat $ A.asListL $ go (sort $ eventPointss cs) A.empty A.empty
     where
-      go :: [Event] -> AVL Circle -> Double -> AVL [Position]
+      go :: [Event] -> AVL Circle -> AVL Circle -> AVL [Position]
       go [e] act _ = A.empty
-      go (Insert c@(Cir _ r) : evl) act rad = (intersects c) `A.join` next
+      go (Insert c : evl) act rad = (intersects c) `A.join` next
         where
           intersects :: Circle -> AVL [Position]
-          intersects c = A.map (circlesIntersections c) iv
-          iv = case r >= rad of
-            True -> (interval c act r)
-            False -> (interval c act rad)
-          next = case r >= rad of
-            True -> go evl (A.push (cordering c) c act) r
-            False -> go evl (A.push (cordering c) c act) rad
-      go (Delete c : evl) act rad = go evl (A.delete (ordering c) act) rad
+          intersects c = A.map (circlesIntersections c) (interval c act rad)
+          next = go evl (A.push (cordering c) c act) (A.push (cordering' c) c rad)
+      go (Delete c : evl) act rad = go evl (A.delete (ordering c) act) (A.delete (ordering' c) rad)
 
 ordering :: Circle -> Circle -> Ordering 
 ordering (Cir (Pos _ y1) _) (Cir (Pos _ y2) _) = compare y1 y2
+
+ordering' :: Circle -> Circle -> Ordering 
+ordering' (Cir (Pos _ _) r1) (Cir (Pos _ _) r2) = compare r1 r2
 
 
 cordering :: Circle -> Circle -> COrdering Circle
 cordering c1 c2 = fstByCC ordering c1 c2
 
-interval :: Circle -> AVL Circle -> Double ->AVL Circle
-interval (Cir ct r) act max_r = takeLE (ordering $ Cir max r) $ takeGE (ordering $ Cir min r) act
+cordering' :: Circle -> Circle -> COrdering Circle
+cordering' c1 c2 = fstByCC ordering' c1 c2
+
+interval :: Circle -> AVL Circle -> AVL Circle ->AVL Circle
+interval (Cir ct r) act rad = takeLE (ordering $ Cir max r) $ takeGE (ordering $ Cir min r) act
                       where (min,max) = (ct <-> r', ct <+> r')
+                            max_r = radius $ assertReadR rad
                             r' = (Pos 0 (r + max_r))
