355f2ae5234a84948defb09bb802d884a5d73598
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2014-04-06 10:07:32 +0200

    cleaning up

--- code/SVG.hs
+++ code/SVG.hs
@@ -3,13 +3,17 @@ module SVG where
 import Circle
 import Position
 
-class Drawable a where
-    draw :: a -> String
-
+-- Settings
 scale = 250.0
 offset = 1.0
 width = 1000
 height = 1000
+background          = "#000000"
+circleColor         = "#ffffff"
+intersectionColor   = "#ff0000"
+
+class Drawable a where
+    draw :: a -> String
 
 data SVGUtil =    Header 
                 | Background
@@ -17,45 +21,50 @@ data SVGUtil =    Header
 
 instance Drawable SVGUtil where
     draw Header
-        = "<svg "
-        ++ "width="
-        ++ "\"" ++ show width ++ "\" "
-        ++ "height="
-        ++ "\"" ++ show height ++ "\" "
-        ++ ">"
+        =   "<svg "
+            ++ " width=\""  ++ show width   ++ "\""
+            ++ " height=\"" ++ show height  ++ "\""
+            ++ " >"
     
     draw Background
-        = "    <rect width=\"100%\" height=\"100%\" fill=\"black\"/>"
+        =   "    <rect "
+            ++ " width=\"100%\""
+            ++ " height=\"100%\""
+            ++ " fill=\""   ++ background   ++ "\""
+            ++ " />"
     
     draw Footer
         = "</svg>"
+
+
 
 -- Define a circle to be drawable
 instance Drawable Circle where
     draw (Cir (Pos x y) r)
         =   "    <circle"
-         ++ " cx=\"" ++ show x' ++ "\""
-         ++ " cy=\"" ++ show y' ++ "\""
-         ++ " r=\""  ++ show r' ++ "\""
-         ++ " stroke=\"white\""
-         ++ " stroke-width=\"1\""
-         ++ " style=\"fill-opacity:0.0;stroke-opacity:1\""
-         ++ " />"
+            ++ " cx=\""        ++ show x'      ++ "\""
+            ++ " cy=\""        ++ show y'      ++ "\""
+            ++ " r=\""         ++ show r'      ++ "\""
+            ++ " stroke=\""    ++ circleColor  ++ "\""
+            ++ " stroke-width=\"1\""
+            ++ " style=\"fill-opacity:0.0;stroke-opacity:1\""
+            ++ " />"
         where
             x' = floor $ (x+offset)*scale
             y' = floor $ (y+offset)*scale
             r' = floor $ r*scale
 
 instance Drawable Position where
-    draw (Pos x y) = "    <circle"
-                        ++ " cx=\"" ++ show x' ++ "\""
-                        ++ " cy=\"" ++ show y' ++ "\""
-                        ++ " r=\""  ++ show r  ++ "\""
-                        ++ " fill=\"red\""
-                        ++ " />"
+    draw (Pos x y)
+        =   "    <circle"
+            ++ " cx=\""     ++ show x'              ++ "\""
+            ++ " cy=\""     ++ show y'              ++ "\""
+            ++ " r=\""      ++ show r               ++ "\""
+            ++ " fill=\""   ++ intersectionColor    ++ "\""
+            ++ " />"
         where
             x' = floor $ (x+offset)*scale
             y' = floor $ (y+offset)*scale
-            r  = 3
+            r  = 2
 
 
