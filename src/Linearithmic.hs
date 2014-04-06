536a57fa1a8f72a80bad1ecd995f40602d4b0d63
Xavier Goás Aguililla <xavier.goas@gmail.com>
2014-04-07 01:24:51 +0200

    algo 3 more efficient now

--- src/Linearithmic.hs
+++ src/Linearithmic.hs
@@ -10,16 +10,21 @@ import Position
 import Vector
 
 intersections :: [Circle] -> [Position]
-intersections cs = L.nub $ concat $ A.asListL $ go (sort $ eventPointss cs) A.empty
+intersections cs = L.nub $ concat $ A.asListL $ go (sort $ eventPointss cs) A.empty 0
     where
-      go :: [Event] -> AVL Circle -> AVL [Position]
-      go [e] act = A.empty
-      go (Insert c : evl) act = (intersects c) `A.join` next
+      go :: [Event] -> AVL Circle -> Double -> AVL [Position]
+      go [e] act _ = A.empty
+      go (Insert c@(Cir _ r) : evl) act rad = (intersects c) `A.join` next
         where
           intersects :: Circle -> AVL [Position]
-          intersects c = A.map (circlesIntersections c) (interval c act)
-          next = go evl (A.push (cordering c) c act)
-      go (Delete c : evl) act = go evl (A.delete (ordering c) act)
+          intersects c = A.map (circlesIntersections c) iv
+          iv = case r >= rad of
+            True -> (interval c act r)
+            False -> (interval c act rad)
+          next = case r >= rad of
+            True -> go evl (A.push (cordering c) c act) r
+            False -> go evl (A.push (cordering c) c act) rad
+      go (Delete c : evl) act rad = go evl (A.delete (ordering c) act) rad
 
 ordering :: Circle -> Circle -> Ordering 
 ordering (Cir (Pos _ y1) _) (Cir (Pos _ y2) _) = compare y1 y2
@@ -28,10 +33,7 @@ ordering (Cir (Pos _ y1) _) (Cir (Pos _ y2) _) = compare y1 y2
 cordering :: Circle -> Circle -> COrdering Circle
 cordering c1 c2 = fstByCC ordering c1 c2
 
-interval :: Circle -> AVL Circle -> AVL Circle
-interval (Cir ct r) act = takeLE (ordering $ Cir max r) $ takeGE (ordering $ Cir min r) act
+interval :: Circle -> AVL Circle -> Double ->AVL Circle
+interval (Cir ct r) act max_r = takeLE (ordering $ Cir max r) $ takeGE (ordering $ Cir min r) act
                       where (min,max) = (ct <-> r', ct <+> r')
-                            r' = (Pos 0 (r + maxRadius act))
-
-maxRadius :: AVL Circle -> Double
-maxRadius act = maximum [r | (Cir _ r) <- asListL act ]
+                            r' = (Pos 0 (r + max_r))
