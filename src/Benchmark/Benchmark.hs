29e14413be7cdb3a280f49ca9d8c7e48bae228cf
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2014-04-13 17:57:29 +0200

    damn you, pure functions!

--- src/Benchmark/Benchmark.hs
+++ src/Benchmark/Benchmark.hs
@@ -1,16 +1,42 @@
 module Benchmark.Benchmark where
 
+import Control.Monad
+import System.IO
+import System.Random
 import Criterion.Main
+import Geometry.Circle
+import Geometry.Position
+import Text.Printf
+import Intersections.Intersections
+import Control.DeepSeq
 
-fib :: Int -> Int
-fib 0 = 0
-fib 1 = 1
-fib n = fib (n-1) + fib (n-2)
-benchFib= defaultMain [
-       bgroup "fib" [ bench "10" $ whnf fib 10
-                    , bench "35" $ whnf fib 35
-                    , bench "37" $ whnf fib 37
-                    ]
-                   ]
-benchmark = do
-   putStrLn "hi"
+instance NFData Position
+instance NFData Circle
+
+-- Generate a random scaled circle
+randomScaledCircle :: Double -> IO Circle
+randomScaledCircle sc = do
+    x <- randomIO :: IO Double
+    y <- randomIO :: IO Double
+    r <- randomIO :: IO Double
+    return $ Cir (Pos x y) (sc*r)
+
+-- Generate n random circles with the radii scaled by sc
+randomScaledCircles :: Int -> Double -> IO [Circle]
+randomScaledCircles n sc = replicateM n $ randomScaledCircle sc
+
+-- Benchmark Intersections :: Algorithmnr -> Nr of circles -> Scale of radii -> IO ()
+benchIntersections :: Int -> Int -> Double -> IO ()
+benchIntersections na nc sc = do
+    cs <- randomScaledCircles nc sc
+    defaultMain [benchC na nc sc cs]
+
+benchC :: Int -> Int -> Double  -> [Circle] -> Benchmark
+benchC na nc sc cs  = bench str $ nf (solve na) cs
+    where str = show na ++ "_" ++ show nc ++ "_" ++ show sc
+
+nas = [ 1..3 ]
+ncs = [ c*10^e | e <- [0..1], c<- [1..9] ]
+scs = [ 10**x | x <- [(-2)..2 ] ]
+
+benchmark = benchIntersections 1 10 1
---
+++ src/Benchmark/Settings.hs
@@ -0,0 +1,3 @@
+module Benchmark.Settings where
+
+
