c6cdc2d57c4d0601dcc4558c0641cae986557ccf
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2014-04-13 16:35:53 +0200

    documented two intersection functions.

--- src/Intersections/Naive.hs
+++ src/Intersections/Naive.hs
@@ -5,11 +5,28 @@ import Data.List
 import Geometry.Circle
 import Geometry.Position
 
+{-
+    Calculate all intersections of a list of circles in O(n^2) time. (n*(n-1)/2)
+    
+    Calculate the intersections of every circle with every other circle.
+-}
 intersections :: [Circle] -> [Position]
+
+-- 0 circles have no intersections.
 intersections [] = []
+
+-- 1 circle has no intersections.
 intersections [c] = []
+
+-- A list of circles may have intersections.
 intersections l = nub $ go l
     where 
+        -- 0 circles have no intersections
         go [] = []
+        
+        -- 1 circle has no intersections.
         go [c] = []
+        
+        -- find the intersections of the first circle with every other circles,
+        -- then go on to the next circle and put all intersections together in a list.
         go (c:cs) = concatMap (circlesIntersections c) cs ++ go cs
--- src/Intersections/Quadratic.hs
+++ src/Intersections/Quadratic.hs
@@ -6,9 +6,40 @@ import Geometry.Circle
 import Geometry.Position
 import Intersections.Event
 
+{-
+    Calculate all intersections of a list of circles in O(n^2) worst case time.
+    
+    This is a sweepline algorithm.
+    The eventpoints for this algorithms are all circle events of the input circles.
+    
+    We go through every eventpoint from left to right,
+    inserting circles for insert events into the status queue
+    and deleting cirlces for delete events.
+
+    For every insertion, we calculate the insertions for each circle in the status queue
+    
+    When we are done with all events, we will have found every intersection.
+-}
 intersections :: [Circle] -> [Position]
+
+-- 0 circles have no intersections.
+intersections [] = []
+
+-- 1 circle has no intersections.
+intersections [c] = 0
+
+-- A list of circles may have intersections.
+-- Remove all duplicates in a list of intersections found by the 'go' function.
+-- Sort the eventpoints of all circles
+-- and perform the 'go' algorithm with an initially empty eventqueue.
 intersections cs = nub $ go (sort $ eventPointss cs) []
     where
+        -- The last event is always a delete event, so there won't be any new intersections.
         go [e] act = []
+        
+        -- For an insert event, insert the circle into the status queue
+        -- and find the intersections with the circles in the eventqueue.
         go (Insert c : evl) act = concatMap (circlesIntersections c) act ++ go evl (c:act)
+        
+        -- For a delete event, delete the circle from the status queue, and move on.
         go (Delete c : evl) act = go evl (delete c act)
