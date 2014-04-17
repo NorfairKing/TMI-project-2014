9b0efcab9a7194e854b03d7c92e7a873615c5186
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2014-04-17 11:54:48 +0200

    done with benchmarks for now

--- src/Benchmark/Settings.hs
+++ src/Benchmark/Settings.hs
@@ -2,13 +2,13 @@ module Benchmark.Settings where
 
 nas, ncs :: [Int]
 nas = [ 1..3 ]
-ncs = [ c*10^e | e <- [0..2], c<- [1..9] ]
+ncs = [ 10,20..200 ]
 
 scs :: [Double]
-scs = [ 10**x | x <- [(-2)..2 ] ]
+scs = [ 10**x | x <- [(-2)..2] ]
 
 ntDefault :: Int
-ntDefault = 100
+ntDefault = 10
 
 resultsDir = "results"
 
